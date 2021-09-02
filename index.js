11e265d8032a1f143b824fa6bb5ef4d43bf64540
Emmanuel Gonzalez <dev.egonzalez1@gmail.com>
2021-09-02 11:58:21 -0500

    deployable

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-const server = require('express')()
+const express = require('express')
+const cors = require('cors')
 
+console.log(process.env.USER)
+console.log(process.env.HOME)
+console.log(process.env.PORT)
 
-server.listen(5000, () => {
-  console.log('listening on port 5000')
+const PORT = process.env.PORT || 5000
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/', (req, res) => {
+  res.send('<h1>Web 45 Rocks!</h1>')
+})
+
+server.get('/api', (req, res) => {
+  res.json({ message: 'Web 45 is awesome!' })
+})
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
 })
